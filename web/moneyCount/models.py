b995fc977ae60e9410f8a2380266d65ce450fa09
Кизилов Максим <kizilovmu@ru2g.ru>
2018-11-19 23:38:15 +0300

    Модели.

--- web/moneyCount/models.py
+++ web/moneyCount/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+# Пользователь
+class User(models.Model):
+    chat_id = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    balance = models.BigIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+# Категория
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+# деньги
+class Money(models.Model):
+    count: models.BigIntegerField()
+    user: models.ManyToManyField(User)
+    category: models.ManyToManyField(Category)
